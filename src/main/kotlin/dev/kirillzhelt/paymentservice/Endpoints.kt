75ffda01e2f379991f449f06530bcfaaafcde513
Kirill Zheltovski <kirillzhelt@gmail.com>
2019-11-19 18:44:24 +0300

    Send updated token to service

--- src/main/kotlin/dev/kirillzhelt/paymentservice/Endpoints.kt
+++ src/main/kotlin/dev/kirillzhelt/paymentservice/Endpoints.kt
@@ -2,4 +2,7 @@ package dev.kirillzhelt.paymentservice
 
 const val SERVICE_REGISTRY = "https://voiteshenkolab3registerservice.azurewebsites.net/WebService.asmx/IsMethodExistsName"
 
-val SERVICES = mapOf("LibraryService" to "", "PlaneTicketService" to "", "TrainTicketService" to "")
+val SERVICES = mapOf(
+        "LibraryService" to "",
+        "PlaneTicketService" to "",
+        "RailwayTicketService" to "https://railwaytickets.azurewebsites.net/api/RailwayTickets/token")
