ab365fad6657d428f05f54a8a54c63a162ea74df
Kirill Zheltovski <kirillzhelt@gmail.com>
2019-11-20 00:00:09 +0300

    Add LibraryService

--- src/main/kotlin/dev/kirillzhelt/paymentservice/Endpoints.kt
+++ src/main/kotlin/dev/kirillzhelt/paymentservice/Endpoints.kt
@@ -3,5 +3,6 @@ package dev.kirillzhelt.paymentservice
 const val SERVICE_REGISTRY = "https://voiteshenkolab3registerservice.azurewebsites.net/WebService.asmx/IsMethodExistsName"
 
 val SERVICES = mapOf(
+        "LibraryService" to "https://voiteshenko-library-service.azurewebsites.net/LibraryService.asmx/AddToken",
         "PlaneTicketService" to "http://voiteshenko-lab3-plane-ticket-service.azurewebsites.net/PlaneTicketService.svc/setToken",
         "RailwayTickets" to "https://railwaytickets.azurewebsites.net/api/RailwayTickets/token")
